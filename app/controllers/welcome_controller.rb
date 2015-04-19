ef8085b3f04115d3d1d461803a0f17ec9c056657
Abram Gutierrez <abram_gutierrez@yahoo.com>
2015-04-19 16:49:38 -0500

    Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ActionController::Base
   layout "application"
+  @events
   
   def index
+	url = "https://www.bikereg.com/api/search"
+	
+	json = JSON.parse(RestClient.get(url));
+	@events = json["MatchingEvents"]
   end
+  
 end
